c628b938db4e2d89731f77099646d2d0a307ccd3
rocky_manobi <tak.koroki@gmail.com>
2014-07-11 13:09:19 +0900

    second first commit

---
+++ web.js
@@ -0,0 +1,46 @@
+var express = require('express') 
+var methodOverride = require('method-override');
+var busboy = require('connect-busboy');
+var EventEmitter = require('events').EventEmitter;
+
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+    
+var ev = new EventEmitter();
+
+app.use(express.static(__dirname + '/public'));
+app.use(busboy()); 
+app.use(methodOverride());
+
+
+var routes = {
+  upload : require( __dirname + '/lib/routes/upload' )(ev,io)
+};
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+app.post('/upload', routes.upload.post );
+
+
+/****** io ******/
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+
+var port = Number(process.env.PORT || 3000);
+http.listen(port, function(){
+  console.log('listening on *:3000');
+});
