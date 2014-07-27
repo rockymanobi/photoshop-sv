14e93fcc50e253d11468feaf39318b7bf5fd1788
rocky_manobi <tak.koroki@gmail.com>
2014-07-27 15:46:04 +0900

    redis対応を優先させるのでごめん、一旦コミット

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,16 @@ function showLoading(){
   },50);
 }
 
+function reflectImage(targetSelector, file){
+
+  console.log(123);
+  var fileReader = new FileReader();
+  fileReader.onload = function(event) {
+    return $(targetSelector).css("background-image", "url("+ event.target.result +")");
+  };
+  return fileReader.readAsDataURL(file);
+}
+
 $(document).ready( function(){
   $('.upload-image-button').on('click',function(){
     showLoading();
@@ -13,17 +23,21 @@ $(document).ready( function(){
   // 写真をとったら画面に反映
   $('input[name=image]').on('change', function () {
 
-    var file, fileReader;
+    console.log(this);
     if (!this.files.length) {
       return;
     }
-    file = this.files[0];
-    $_img = $("#message-image");
-    fileReader = new FileReader();
-    fileReader.onload = function(event) {
-      return $('.bummybtn').css("background-image", "url("+ event.target.result +")");
-    };
-    return fileReader.readAsDataURL(file);
+    var file = this.files[0];
 
+    // file size validation
+    var fileSizeMB = file.size / 1024 / 1024;
+    if ( fileSizeMB <= 3 ) { 
+    }else{
+      alert( "デカい！");
+      return; 
+    }
+
+    // OKだったら反映
+    reflectImage( '.bummybtn', file );
   });
 });
